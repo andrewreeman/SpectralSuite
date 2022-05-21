616def8e146889eebf81c6efde555aff82f41f2b
Andrew Reeman <andrew.reeman@gmail.com>
2022-05-21 21:20:42 +0100

    baseprocessor is a phase vocoder

--- SpectralSuiteBuild/BaseProcessor.cpp
+++ SpectralSuiteBuild/BaseProcessor.cpp
@@ -1,11 +1,13 @@
 #include "BaseProcessor.h"
+#include "../../shared/PhaseVocoder.h"
+#include "../../shared/PhaseBuffer.h"
 
 #include "../../shared/SpectralAudioPlugin.h"
 #include "../../shared/utilities.h"
 
-class emptyProcess : public StandardFFTProcessor {
+class emptyProcess : public PhaseVocoder {
 public:
-    emptyProcess(int fftSize, int hopSize, int offset, int sampleRate) : StandardFFTProcessor(fftSize, hopSize, offset, sampleRate) {}
+    emptyProcess(int fftSize, int hopSize, int offset, int sampleRate, std::shared_ptr<PhaseBuffer> phaseBuffer) : PhaseVocoder(fftSize, hopSize, offset, sampleRate, phaseBuffer) {}
     
     void spectral_process(const PolarVector &in, PolarVector &out, int bins) override {
         for(int i=0; i<bins; ++i) {
@@ -16,7 +18,7 @@ public:
 
 std::unique_ptr<StandardFFTProcessor> BaseProcessor::createSpectralProcess(int index, int fftSize, int hopSize, int sampleRate, int numOverlaps, int channel, int channelCount)
 {
-    return std::make_unique<emptyProcess>(fftSize, hopSize, hopSize * (index%numOverlaps), (int)sampleRate);
+    return std::make_unique<emptyProcess>(fftSize, hopSize, hopSize * (index%numOverlaps), (int)sampleRate, this->getPhaseBuffer());
 }
 
 void BaseProcessor::prepareProcess(StandardFFTProcessor * spectralProcessor)
