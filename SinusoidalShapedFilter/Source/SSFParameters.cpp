5d7aba0c4a68b39f43e49cff6c64d4f3d70c3324
Andrew Reeman <andrew.reeman@gmail.com>
2022-08-01 19:12:18 +0100

    formatted to xcodes horrible style

--- SinusoidalShapedFilter/Source/SSFParameters.cpp
+++ SinusoidalShapedFilter/Source/SSFParameters.cpp
@@ -2,28 +2,39 @@
 
 SSFParameters::SSFParameters(AudioProcessor * processor) : PluginParameters(processor) {
     const String freqHertzLabel = "Hz";
-    createAndAddParameter(std::make_unique<AudioParameterFloat>(
+    createAndAddParameter(
+                          std::make_unique<AudioParameterFloat>(
                                                                 ParameterID("freq", 1),
                                                                 "Frequency",
-                                                                NormalisableRange<float>(0.0f, 10.0f), 7.0f, "",
+                                                                NormalisableRange<float>(0.0f, 10.0f),
+                                                                7.0f,
+                                                                "",
                                                                 AudioProcessorParameter::Category::genericParameter,
                                                                 [freqHertzLabel](float v, int) { return String(v, 2) + freqHertzLabel; },
                                                                 [freqHertzLabel](const String& text) { return text.dropLastCharacters(freqHertzLabel.length()).getFloatValue(); }
-                                                                ));
+                                                                )
+                          );
     
-       
-    createAndAddParameter(std::make_unique<AudioParameterFloat>(
+    
+    createAndAddParameter(
+                          std::make_unique<AudioParameterFloat>(
                                                                 ParameterID("width", 1),
                                                                 "Width",
-                                                                NormalisableRange<float>(0.0f, 1.0f), 0.9f, "",
+                                                                NormalisableRange<float>(0.0f, 1.0f),
+                                                                0.9f,
+                                                                "",
                                                                 AudioProcessorParameter::Category::genericParameter
-                                                                ));
+                                                                )
+                          );
     
-     //    const String piLabel = " pi";
-    createAndAddParameter(std::make_unique<AudioParameterFloat>(
+    //    const String piLabel = " pi";
+    createAndAddParameter(
+                          std::make_unique<AudioParameterFloat>(
                                                                 ParameterID("phase", 1),
                                                                 "Phase",
-                                                                NormalisableRange<float>(0.0f, 1.0f), 0.5f, "",
+                                                                NormalisableRange<float>(0.0f, 1.0f),
+                                                                0.5f,
+                                                                "",
                                                                 AudioProcessorParameter::Category::genericParameter//,
                                                                 //        [piLabel](float v, int) { return String(v, 2) + piLabel; },
                                                                 //        [piLabel](const String& text) { return text.dropLastCharacters(piLabel.length()).getFloatValue(); }
