4e09a9b038fce86a2d42260f50bd4a309c208a80
Andrew Reeman <andrew.reeman@gmail.com>
2024-06-08 17:34:37 +0100

    validating phase lock

--- auValidate.sh
+++ auValidate.sh
@@ -1,1 +1,5 @@
-auval -strict -v aumf SPFS STPW -r 5 -s 30
+#auval -strict -v aumf SPFS STPW 
+#auval -strict -v aumf SPBS STPW
+#auval -strict -v aumf SPFM STPW
+#auval -strict -v aumf SPMO STPW
+auval -strict -v aumf SPPL STPW
