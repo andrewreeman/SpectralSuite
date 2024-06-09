85549e9cd02d60c5c5ad6e936d4dd5094bd542bc
Andrew Reeman <andrew.reeman@gmail.com>
2024-06-09 12:08:07 +0100

    Added SSF to au validation

--- auValidate.sh
+++ auValidate.sh
@@ -1,5 +1,17 @@
+# Frequency shifter
 #auval -strict -v aumf SPFS STPW 
+
+# Bin Scrambler
 #auval -strict -v aumf SPBS STPW
+
+# Frequency magnet
 #auval -strict -v aumf SPFM STPW
+
+# Morph
 #auval -strict -v aumf SPMO STPW
-auval -strict -v aumf SPPL STPW
+
+# Phase lock
+#auval -strict -v aumf SPPL STPW
+
+# Sinusoidal Sharped Filer
+auval -strict -v aumf SPSS STPW
