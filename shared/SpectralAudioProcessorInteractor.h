b5bcd2edc19eaa4af4a90a809216680b9e8a542c
Andrew Reeman <andrew.reeman@gmail.com>
2024-09-22 14:32:43 +0100

    fixed formatting

--- shared/SpectralAudioProcessorInteractor.h
+++ shared/SpectralAudioProcessorInteractor.h
@@ -13,29 +13,38 @@ public:
     SpectralAudioProcessorInteractor(int numOverlaps);
     virtual ~SpectralAudioProcessorInteractor(){}
 
-	int getHopSize() const { return m_fftHopSize; }	
-	int getFftSize();
-	int getSampleRate() const { return m_sampleRate; }
+    int getHopSize() const { return m_fftHopSize; }	
+    int getFftSize();
+    int getSampleRate() const { return m_sampleRate; }
     int getNumOverlaps() const { return m_numOverlaps; }
     std::shared_ptr<PhaseBuffer> getPhaseBuffer() const { return m_phaseBuffer; }
 
-	virtual std::unique_ptr<StandardFFTProcessor> createSpectralProcess(
-        int index, int fftSize, int hopSize, int sampleRate, int numOverlaps, int channel, int channelCount
-    ) = 0;
- 
+    virtual std::unique_ptr<StandardFFTProcessor> createSpectralProcess(
+		    int index, 
+		    int fftSize,
+		    int hopSize,
+		    int sampleRate,
+		    int numOverlaps,
+		    int channel,
+		    int channelCount) = 0;
+
     virtual void receivedMidi(MidiBuffer& midiBuffer){};
-	virtual void prepareProcess(StandardFFTProcessor*) {}	
-	virtual void onFftSizeChanged() {};	
+    virtual void prepareProcess(StandardFFTProcessor*) {}	
+    virtual void onFftSizeChanged() {};	
 	
     /// Can be overridden if needs to query plugin
-    virtual void process(SpectralAudioPlugin* plugin, std::vector<std::vector<float>>* input, std::vector<std::vector<float>>* output)
+    virtual void process(
+		    SpectralAudioPlugin* plugin,
+		    std::vector<std::vector<float>>* input,
+		    std::vector<std::vector<float>>* output)
     {
-        this->process(input, output);
+	    this->process(input, output);
     }
+
     virtual void process(std::vector<std::vector<float>>* input, std::vector<std::vector<float>>* output);
 
-	void prepareToPlay(int fftSize, int sampleRate, int channelCount);			
-	void setFftSize(int fftSize);
+    void prepareToPlay(int fftSize, int sampleRate, int channelCount);
+    void setFftSize(int fftSize);
     void usePvoc(bool usePvoc) { m_phaseBuffer->setUsePvoc(usePvoc); };
     void setNumOverlaps(int newOverlapCount);
     void setWindowType(FftWindowType newWindowType);
@@ -47,8 +56,8 @@ protected:
 
 private:
     int m_numOverlaps;
-	int m_sampleRate;
-	int m_fftHopSize;
+    int m_sampleRate;
+    int m_fftHopSize;
     int m_numChans;
     int m_fftSize;
     bool m_isPreparingToPlay;
