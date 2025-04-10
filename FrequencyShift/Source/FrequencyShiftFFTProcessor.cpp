fc3e7d2a55ab603488c401d3a17151642c6c5d9d
Andrew Reeman <andrew.reeman@gmail.com>
2025-04-10 22:44:28 +0100

    Scaling frequency

--- FrequencyShift/Source/FrequencyShiftFFTProcessor.cpp
+++ FrequencyShift/Source/FrequencyShiftFFTProcessor.cpp
@@ -1,26 +1,34 @@
 #include "FrequencyShiftFFTProcessor.h"
 
-FrequencyShiftFFTProcessor::FrequencyShiftFFTProcessor(int size, int hops, int offset, int sRate, std::shared_ptr<PhaseBuffer> phaseBuffer)
-    : PhaseVocoder(size, hops, offset, sRate, phaseBuffer), m_shift(0), m_binShift(0), m_shiftStartIndex(0), m_shiftEndIndex(0)
+FrequencyShiftFFTProcessor::FrequencyShiftFFTProcessor(const int size, const int hops, const int offset, const int sRate, const std::shared_ptr<PhaseBuffer> &phaseBuffer)
+    : PhaseVocoder(size, hops, offset, sRate, phaseBuffer), m_shift(0), m_scale(1.f), m_binShift(0), m_shiftStartIndex(0), m_shiftEndIndex(0)
 {
     recalculateInternalParameters();
 }
 
 void FrequencyShiftFFTProcessor::spectral_process(const PolarVector &in, PolarVector &out, int) {
-    if (m_binShift == 0) {
-        for (size_t i = 0; i < (size_t)in.size(); ++i) {
+    if (m_binShift == 0 && approximatelyEqual(m_scale, 0.f)) {
+        for (size_t i = 0; i < in.size(); ++i) {
             out[i] = in[i];
         }
         return;
     }
 
-    auto start = (size_t)m_shiftStartIndex;
-    auto end = (size_t)m_shiftEndIndex;
-    long binShift = m_binShift;
-    
-    std::fill(out.begin(), out.end(), Polar<FftDecimal>(0.f, 0.f));
-    for (auto i = start; i < end; ++i) {
-        out[i + (size_t)binShift] = in[i];
+    const auto start = static_cast<size_t>(m_shiftStartIndex);
+    const auto end = static_cast<size_t>(m_shiftEndIndex);
+    const long binShift = m_binShift;
+
+    std::fill(out.begin(), out.end(), Polar(0.f, 0.f));
+    if (approximatelyEqual(m_scale, 0.f)) {
+        for (auto i = 0; i < in.size(); ++i) {
+            out[static_cast<size_t>(static_cast<float>(i) * m_scale)] = in[i];
+        }
+    }
+
+    if (m_binShift != 0) {
+        for (auto i = start; i < end; ++i) {
+            out[i + static_cast<size_t>(binShift)] = in[i];
+        }
     }
 }
 
@@ -41,9 +49,9 @@ void FrequencyShiftFFTProcessor::recalculateInternalParameters() {
     }
         
     int halfFftSize = getFFTSize() / 2;
-    float binWidth = (float)getFFTSize() / (float)getSampleRate();
+    float binWidth = static_cast<float>(getFFTSize()) / static_cast<float>(getSampleRate());
     int shiftFrequency = m_shift;
-    m_binShift = (int) ((float)shiftFrequency * (float)binWidth);
+    m_binShift = static_cast<int>(static_cast<float>(shiftFrequency) * binWidth);
     
     m_shiftStartIndex = m_binShift > 0 ? 0 : abs(m_binShift);
     m_shiftEndIndex = m_binShift > 0 ? halfFftSize - m_binShift : halfFftSize;
